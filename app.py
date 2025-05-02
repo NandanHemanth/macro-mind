8c6bfd16eb60859fa48ce597ab0cb27384e9c556
Murali Sai <murali140824@gmail.com>
2025-05-01 22:20:21 -0400

    initialisation

--- app.py
+++ app.py
@@ -1,27 +1,24 @@
-# app.py
-
 import streamlit as st
-from video_recorder import VideoRecorder
 
-def main():
-    st.title("🎥 Video Recorder")
-    
-    st.write("This is a basic video recorder app. You can start a 30-second recording and save it to the `./recording` folder.")
-    
-    if st.button("Start Recording"):
-        st.write("Recording started... 🏃‍♂️")
-        
-        # Create an instance of VideoRecorder
-        recorder = VideoRecorder(duration=30)
-        
-        # Start recording
-        recorder.start_recording()
-        
-        st.success("Recording finished and saved successfully! 🎬")
-        st.write("You can check the saved video in the `./recording` folder.")
-    
-    if st.button("Stop Recording"):
-        st.write("Recording stopped manually. 🎬")
+# Set page configuration
+st.set_page_config(page_title="MacroMind", page_icon="💪", layout="wide")
 
-if __name__ == "__main__":
-    main()
+import requests
+import json
+import os
+import subprocess
+import sqlite3
+from streamlit_lottie import st_lottie
+from PIL import Image
+
+class UserProfileManager:
+    def __init__(self, path="./database/user_data.json"):
+        self.path = path
+        self.default_data = {
+            "name": "",
+            "height": 170,
+            "weight": 70,
+            "goal": "",
+            "dietary_restriction": ""
+        }
+        self.init_data()
